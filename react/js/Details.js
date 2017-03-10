a2b33ecf5b6365c95ddced28f4a3152827c3b23a
davismartin <davisshippmartin@gmail.com>
2017-03-10 13:27:05 -0800

    simplify header

--- react/js/Details.js
+++ react/js/Details.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Header from './Header'
+
 const {shape, string} = React.PropTypes
 const Details = React.createClass({
   propTypes: {
@@ -13,9 +15,7 @@ const Details = React.createClass({
     const {title, description, year, poster, trailer} = this.props.show
     return (
       <div className='details'>
-        <header>
-          <h1>svideo</h1>
-        </header>
+        <Header />
         <section>
           <h1>{title}</h1>
           <h2>({year})</h2>
@@ -26,9 +26,9 @@ const Details = React.createClass({
           <iframe src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
             frameBorder='0' allowFullScreen />
         </div>
-        <pre><code>
+        {/* <pre><code>
           {JSON.stringify(this.props, null, 4)}
-        </code></pre>
+        </code></pre> */}
       </div>
     )
   }
