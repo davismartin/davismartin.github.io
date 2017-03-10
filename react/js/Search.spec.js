9971bd0b166b49f551306064b6ca10861c28004e
davismartin <davisshippmartin@gmail.com>
2017-03-10 10:36:54 -0800

    setup testing with jest

---
+++ react/js/Search.spec.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import {shallowToJson} from 'enzyme-to-json'
+import Search from './Search'
+import ShowCard from './ShowCard'
+import preload from '../public/data.json'
+
+
+test('Search snapshot test', () => {
+  const component = shallow(<Search />)
+  const tree = shallowToJson(component)
+  expect(tree).toMatchSnapshot()
+})
+
+test('Search should render a ShowCard for each show', () => {
+  const component = shallow(<Search />)
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length)
+})
+
+test('Search should render correct amount of shows based on search', () => {
+  const searchWord = 'house'
+  const component = shallow(<Search />)
+  component.find('input').simulate('change', {target: {value: searchWord}})
+  const showCount = preload.shows.filter((show) => {
+    return `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0 }).length
+  expect(showCount).toEqual(component.find(ShowCard).length)
+})
