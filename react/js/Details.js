0aea006ac2ba5e198dc2037c823c80c60c32f752
davismartin <davisshippmartin@gmail.com>
2017-03-10 12:28:52 -0800

    make details page

---
+++ react/js/Details.js
@@ -0,0 +1,37 @@
+import React from 'react'
+const {shape, string} = React.PropTypes
+const Details = React.createClass({
+  propTypes: {
+    show: shape({
+      title: string,
+      year: string,
+      poster: string,
+      trailer: String()
+    })
+  },
+  render () {
+    const {title, description, year, poster, trailer} = this.props.show
+    return (
+      <div className='details'>
+        <header>
+          <h1>svideo</h1>
+        </header>
+        <section>
+          <h1>{title}</h1>
+          <h2>({year})</h2>
+          <img src={`/public/img/posters/${poster}`} />
+          <p>{description}</p>
+        </section>
+        <div>
+          <iframe src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+            frameBorder='0' allowFullScreen />
+        </div>
+        <pre><code>
+          {JSON.stringify(this.props, null, 4)}
+        </code></pre>
+      </div>
+    )
+  }
+})
+
+export default Details
--- react/js/Search.js
+++ react/js/Search.js
@@ -1,8 +1,14 @@
 import React from 'react'
-import preload from '../public/data.json'
 import ShowCard from './ShowCard'
+const {arrayOf, shape, string} = React.PropTypes
 
 const Search = React.createClass({
+  propTypes: {
+    shows: arrayOf(shape({
+      title: string,
+      description: string
+    }))
+  },
   getInitialState () {
     return {
       searchTerm: ''
@@ -19,7 +25,7 @@ const Search = React.createClass({
           <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
         </header>
         <div>
-          {preload.shows
+          {this.props.shows
             .filter((show) => {
               return `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
             })
